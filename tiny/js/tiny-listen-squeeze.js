2a9ef50f51b2889628c1e3e7c226f0bb75509390
Benjamin Muñoz <munoz@benjamins-mbp-2.wireless.concordia.ca>
2019-07-25 19:06:04 -0400

    mp3zz

--- tiny/js/tiny-listen-squeeze.js
+++ tiny/js/tiny-listen-squeeze.js
@@ -4,22 +4,19 @@ var mic;
 var analyzer;
 
 function preload() {
-work = loadSound('assets/edge-operators/embers.wav');
-arounds = loadSound('assets/edge-operators/embers.wav');
-are = loadSound('assets/edge-operators/embers.wav');
-the = loadSound('assets/tinies/tiny.sounds.wav');
-best = loadSound('assets/tinies/tiny.sounds.wav');
-nice = loadSound('assets/tinies/tiny.sounds.wav');
-hehe = loadSound('assets/tinies/tiny.sounds.wav', listenCreepy());
+work = loadSound('assets/edge-operators/embers.mp3');
+arounds = loadSound('assets/edge-operators/embers.mp3');
+are = loadSound('assets/edge-operators/embers.mp3');
+the = loadSound('assets/tinies/tiny.sounds.mp3');
+best = loadSound('assets/tinies/tiny.sounds.mp3');
+nice = loadSound('assets/tinies/tiny.sounds.mp3');
+hehe = loadSound('assets/tinies/tiny.sounds.mp3', listenCreepy());
 }
 
 function setup() {
   frameRate(60);
   createCanvas(710, 200);
   background(255);
-  textSize(30);
-  textAlign(CENTER);
-
   edgeOps = [work, arounds, are];
   tinies = [the, best, nice, hehe];
   edgeOptions = loadStrings('assets/edge-operators.txt');
@@ -52,22 +49,22 @@ let i = 0;
 let j = 0;
 
 function draw() {
-
   //listening creepy to trigger sound function nice
   var volume = mic.getLevel();
   let avg = analyzer.getLevel();
   //play soundz creepy
   if (abs(volume-avg) > 0.005 && (frameCount) % 20 === 0) {
-    trigSound(random(0.0, 0.3), 1.0, volume*5, random(0.7, 2.5), tinies[(i % 3)]);
+    trigSound(random(0.0, 0.3), 1.0, (1/(1+exp(-5*(volume-0.25)))), random(0.7, 2.5), tinies[(i % 3)]);
+    //1/(1+e**(-k(x-x0)))
     i++;
   } else if ((frameCount) % 20 === 0) {
     trigSound(random(0.0, 0.3), random(0.75, 1.15), volume, random(0.1, 0.2), edgeOps[(j % 3)]);
     j++;
   }
-
   //keep track of time...
   if(frameCount % 60 === 0){
 	   time = time + 1;
+     console.log(time);
    }
   //squeeze thresholds closer over 60s: reset timer if thresholds are crossed
   let squeeze = 0.1**(1+time/30);
@@ -81,25 +78,9 @@ function draw() {
     trigSound(0, 1.0, 1.0, random(4.0, 6.0), tinies[3]);
     time = 0;
   }
-
-  //for visualizing thresholds:
-  let yvolume = map(volume, 0, 1, height, 0);
-  let yupper = map((avg+squeeze), 0, 1, height, 0);
-  let ylower = map((avg-squeeze), 0, 1, height, 0);
-
-  noStroke();
-  fill(175);
-  rect(0, 0, 20, height);
-  // Then draw a rectangle on the graph, sized according to volume
-  stroke(0, 0, 0);
-  line(0, yvolume, 19, yvolume);
-  stroke(128,0,128);
-  line(0, yupper, 19, yupper);
-  stroke(255, 204, 0);
-  line(0, ylower, 19, ylower);
-  text(time, width / 2, height / 2);
 }
 
+//to change soundz
 function mousePressed(){
     //generate random indices
     let whodat = floor(random(edgeOptions.length));
